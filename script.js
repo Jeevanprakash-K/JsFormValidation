3c60eb2fb2fd1bf6247330182b44ca7f5dbc6c43
Jeevanprakash-K <146735666+Jeevanprakash-K@users.noreply.github.com>
2024-08-21 20:01:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,13 +5,16 @@ function validateForm() {
   var contact = document.getElementById("contact").value.trim();
   var address = document.getElementById("address").value.trim();
   var city = document.getElementById("city").value.trim();
+  var flag = true;
 
   if (fname === "") {
     alert("First Name must be filled out");
+    flag = false;
   }
 
   if (lname === "") {
     alert("Last Name must be filled out");
+    flag = false
   }
 
   if (email === "") {
@@ -21,23 +24,31 @@ function validateForm() {
   var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
   if (!emailPattern.test(email)) {
     alert("Please enter a valid email address");
+    flag = false;
   }
 
   if (contact === "") {
     alert("Contact must be filled out");
+    flag = false;
   }
 
   var contactPattern = /^\d{10}$/;
   if (!contactPattern.test(contact)) {
     alert("Contact must be a 10-digit number");
+    flag = false;
   }
 
   if (address === "") {
     alert("Address must be filled out");
+    flag = false;
   }
 
   if (city === "") {
     alert("City must be filled out");
+    flag = false;
   }
-  alert("Success");
+
+  if(flag == true){
+    alert("Success");
+  }
 }
